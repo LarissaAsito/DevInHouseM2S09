07c3173fcca8d9d53d34b76f0368663d7fbae3c3
Larissa <larissa.asito@gmail.com>
2023-04-01 02:01:15 -0300

    Adicionando o exercicio 5

---
+++ mercado-senai/src/main/java/org/devinhouse/semana09/mercadosenai/MercadoSenaiApplication.java
@@ -0,0 +1,13 @@
+package org.devinhouse.semana09.mercadosenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoSenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoSenaiApplication.class, args);
+	}
+
+}
---
+++ mercado-senai/src/test/java/org/devinhouse/semana09/mercadosenai/MercadoSenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devinhouse.semana09.mercadosenai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoSenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
